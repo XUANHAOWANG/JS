958a6fa5920aaf8db7a52e31518856fb9145c108
unknown <438164432@qq.comm>
2020-09-16 13:20:53 +1200

    js

---
+++ index.js
@@ -0,0 +1,34 @@
+console.log("hello world")
+
+console.log(Math.floor(100.41241))
+
+console.log(Math.ceil(100.41241))
+
+var name = "xuanhao " 
+console.log(name)
+
+var LastName = "Wang"
+console.log(LastName)
+
+var FullName = name + LastName
+console.log(FullName)
+ var age ="25"
+ console.log(age)
+
+console.log("my name is " + FullName)
+
+var address = "hardyST"
+console.log(address)
+
+let firstName = "Xuanhao"
+let lastName ="Wang"
+let fullName = firstName + lastName
+console.log(fullName)
+
+
+console.log(`Hellow! I am ${firstName} and my last name is ${lastName} `)
+
+var homeTown = "Deyang"
+console.log(homeTown)
+
+console.log( `Hellow, My first name is ${firstName} and my last name is ${lastName} ,so my full name is ${fullName}, my dress is ${address},my home town is ${homeTown}, nice to meet you`)
